ed26591ba5f92ec723d4d4d0de640f9743b9f76b
Deepak Guleria <dguleria07@gmail.com>
2025-10-30 17:12:38 +0530

    file picker added

--- app/src/main/java/com/example/happydocx/ui/Screens/SignUpForms/form_two_screen.kt
+++ app/src/main/java/com/example/happydocx/ui/Screens/SignUpForms/form_two_screen.kt
@@ -1,6 +1,13 @@
 package com.example.happydocx.ui.Screens.SignUpForms
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,10 +16,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
@@ -21,23 +30,31 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
 
 @Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Form_Two_Screen() {
+
 
     val gradient_colors = Brush.linearGradient(
         listOf(
@@ -99,65 +116,107 @@ fun Form_Two_Screen() {
                 )
             }
             // adding the profile photo
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 4.dp, vertical = 8.dp)) {
-                Text("Profile Photo", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp, vertical = 8.dp)
+            ) {
+                Text(
+                    "Profile Photo",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
                 Text("*", color = Color.Red)
             }
             // know i add the add box section
-            MyDashedBox()
+            MyDashedBox(fileType = "image")
 
 
             // adding Signature
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 4.dp, vertical = 8.dp)) {
-                Text("Signature", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp, vertical = 8.dp)
+            ) {
+                Text(
+                    "Signature",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
                 Text("*", color = Color.Red)
             }
             // know i add the add box section
-            MyDashedBox()
+            MyDashedBox("image")
 
             // adding the Doctor ID Proof
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 4.dp, vertical = 8.dp)) {
-                Text("Doctor ID Proof", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp, vertical = 8.dp)
+            ) {
+                Text(
+                    "Doctor ID Proof",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
                 Text("*", color = Color.Red)
             }
             // know i add the add box section
-            MyDashedBox()
+            MyDashedBox(fileType = "document")
 
             // adding the Doctor License
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 4.dp, vertical = 8.dp)) {
-                Text("Doctor License", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp, vertical = 8.dp)
+            ) {
+                Text(
+                    "Doctor License",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
                 Text("*", color = Color.Red)
             }
             // know i add the add box section
-            MyDashedBox()
+            MyDashedBox("document")
 
             // adding the MBBS Certificate
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 4.dp, vertical = 8.dp)) {
-                Text("MBBS Certificate", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp, vertical = 8.dp)
+            ) {
+                Text(
+                    "MBBS Certificate",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
                 Text("*", color = Color.Red)
             }
             // know i add the add box section
-            MyDashedBox()
+            MyDashedBox("document")
 
             // adding the Experience Certificate
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 4.dp, vertical = 8.dp)) {
-                Text("Experience Certificate", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp, vertical = 8.dp)
+            ) {
+                Text(
+                    "Experience Certificate",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
                 Text("*", color = Color.Red)
             }
             // know i add the add box section
-            MyDashedBox()
+            MyDashedBox("document")
 
 
             // Adding Button
@@ -183,30 +242,86 @@ fun Form_Two_Screen() {
     }
 }
 
-
-
 @Composable
-fun MyDashedBox() {
+fun MyDashedBox(
+    fileType:String
+) {
     val stroke =
         Stroke(width = 2f, pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f))
+    //   Each instance will remember its own selected image
+    val selectedImageUri = remember { mutableStateOf<Uri?>(null) }
+    // picker launcher for this instance
+    val imagePicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) {uri->
+        selectedImageUri.value = uri
+    }
+
+    // document picker
+    val documentPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri->
+        selectedImageUri.value = uri
+    }
+
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 8.dp)
             .drawBehind {
                 drawRoundRect(color = Color.Black, style = stroke)
+            }
+            .clickable{
+                if(fileType=="image"){
+                    imagePicker.launch(
+                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                    )
+                }else{
+                    documentPicker.launch("application/pdf")
+                }
             },
         contentAlignment = Alignment.Center
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        )
-        {
-            Text(textAlign = TextAlign.Center, text = "Upload file here", fontSize = 14.sp, color = Color.Black)
-            Text(textAlign = TextAlign.Center, text = "PNG, JPG up to 2MB", fontSize = 12.sp, color = Color.Black)
+        // if image exists
+        if(selectedImageUri.value!=null){
+            if(fileType=="image") {
+                val painter = rememberAsyncImagePainter(
+                    ImageRequest.Builder(LocalContext.current)
+                        .data(selectedImageUri.value)
+                        .build()
+                )
+
+                Image(
+                    painter = painter,
+                    contentDescription = "Selected Image",
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    contentScale = ContentScale.Crop
+                )
+            }else{
+                Text(
+                    text = "${selectedImageUri.value?.lastPathSegment}",
+                    fontSize = 12.sp,
+                    color = Color.Black
+                )
+            }
+        }else{
+            Column (horizontalAlignment = Alignment.CenterHorizontally){
+                    Text(
+                        text = "Upload file here",
+                        fontSize = 14.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = "PNG, JPG up to 2MB",
+                        fontSize = 12.sp,
+                        color = Color.Gray,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
         }
-    }
 }
