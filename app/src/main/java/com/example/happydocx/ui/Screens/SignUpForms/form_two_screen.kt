5ed5ef383b94797369acf0f1cde2b0bac47d9514
Deepak Guleria <dguleria07@gmail.com>
2025-10-30 14:36:04 +0530

    form2-UI-Done

---
+++ app/src/main/java/com/example/happydocx/ui/Screens/SignUpForms/form_two_screen.kt
@@ -0,0 +1,212 @@
+package com.example.happydocx.ui.Screens.SignUpForms
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Form_Two_Screen() {
+
+    val gradient_colors = Brush.linearGradient(
+        listOf(
+            Color(0xff586AE5), Color(0xff717FE8),
+            Color(0xff7785E9)
+        )
+    )
+    val scrollBehaviour = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Column {
+                        Text(
+                            "Document Upload",
+                            fontWeight = FontWeight.ExtraBold,
+                            color = Color.White
+                        )
+                        Text(
+                            "Upload required documents to complete your profile",
+                            color = Color.White,
+                            fontSize = 16.sp
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent,
+                    scrolledContainerColor = Color.Transparent // keep the same gradient
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(gradient_colors),
+                scrollBehavior = scrollBehaviour
+            )
+
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .verticalScroll(rememberScrollState())
+                .padding(paddingValues = innerPadding)
+        ) {
+            // section 4
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp, vertical = 10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                CustomNumberDisplay(4)
+                Spacer(Modifier.width(8.dp))
+                Text(
+                    "Required Documents",
+                    fontSize = 18.sp,
+                    color = Color(0xff4f61e3),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            // adding the profile photo
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 4.dp, vertical = 8.dp)) {
+                Text("Profile Photo", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+                Text("*", color = Color.Red)
+            }
+            // know i add the add box section
+            MyDashedBox()
+
+
+            // adding Signature
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 4.dp, vertical = 8.dp)) {
+                Text("Signature", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+                Text("*", color = Color.Red)
+            }
+            // know i add the add box section
+            MyDashedBox()
+
+            // adding the Doctor ID Proof
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 4.dp, vertical = 8.dp)) {
+                Text("Doctor ID Proof", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+                Text("*", color = Color.Red)
+            }
+            // know i add the add box section
+            MyDashedBox()
+
+            // adding the Doctor License
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 4.dp, vertical = 8.dp)) {
+                Text("Doctor License", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+                Text("*", color = Color.Red)
+            }
+            // know i add the add box section
+            MyDashedBox()
+
+            // adding the MBBS Certificate
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 4.dp, vertical = 8.dp)) {
+                Text("MBBS Certificate", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+                Text("*", color = Color.Red)
+            }
+            // know i add the add box section
+            MyDashedBox()
+
+            // adding the Experience Certificate
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 4.dp, vertical = 8.dp)) {
+                Text("Experience Certificate", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+                Text("*", color = Color.Red)
+            }
+            // know i add the add box section
+            MyDashedBox()
+
+
+            // Adding Button
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                FilledTonalButton(
+                    onClick = {},
+                    modifier = Modifier.padding(horizontal = 15.dp, vertical = 30.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xff16a34a),
+                        contentColor = Color.White
+                    ),
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Text(
+                        "Complete Registration",
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(8.dp),
+                        fontSize = 13.sp
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun MyDashedBox() {
+    val stroke =
+        Stroke(width = 2f, pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f))
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp)
+            .drawBehind {
+                drawRoundRect(color = Color.Black, style = stroke)
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Text(textAlign = TextAlign.Center, text = "Upload file here", fontSize = 14.sp, color = Color.Black)
+            Text(textAlign = TextAlign.Center, text = "PNG, JPG up to 2MB", fontSize = 12.sp, color = Color.Black)
+        }
+    }
+}
